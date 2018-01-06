c8744265710e6bbd2f2455f768db2b70d2efd938
Liesbeth de Keyzer <liesbeth.de.keyzer@lightspeedretail.com>
2018-01-06 12:07:59 +0100

    first commit

---
+++ src/main/java/java/com/springsecurityexample/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package java.com.springsecurityexample.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer
+{
+	
+}
